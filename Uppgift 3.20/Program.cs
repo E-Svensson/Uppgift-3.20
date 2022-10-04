d351571fc51eb3390b0842a3ade63df5e0edaa4a
edvin <edvin@DESKTOP-QHD6AI8>
2022-10-04 19:23:58 +0200

    Add project files.

---
+++ Uppgift 3.20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ett ord: "); string ord1 = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Skriv in ett till ord: "); string ord2 = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.Write("Skriv in ett tredje ord: "); string ord3 = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            string[] ord = { ord1, ord2, ord3 };
+
+            Array.Sort(ord);
+
+            Console.WriteLine($"Det ordet som kommer först i bokstavsordning ordning blir då \"{ord[0]}\".");
+        }
+    }
+}
